70b3df190b6facc84003971b9892abd7539bff20
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-04-11 10:57:42 -0300

    feat(rebeca): favicon and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,32 @@ import Header from "@/assets/components/layout/header/header";
 import Section from "@/assets/components/layout/section/section";
 import Services from "@/assets/components/layout/services/services";
 import Input from "@/assets/components/ui/input/input";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Header />
-        <div className="mt-6">
-          <Input />
+      <Head>
+        <title>SIAPESQ</title>
+        <meta name="description" content="Acesso à plataforma SIAPESQ" />
+      </Head>
+      <main>
+        <div>
+          <Header />
+          <div className="mt-6">
+            <Input />
+          </div>
+          <div className="mt-6">
+            <Section />
+          </div>
+          <div className="mt-6">
+            <Services />
+          </div>
+          <div className="mt-6">
+            <Footer />
+          </div>
         </div>
-        <div className="mt-6">
-          <Section />
-        </div>
-        <div className="mt-6">
-          <Services />
-        </div>
-        <div className="mt-6">
-          <Footer />
-        </div>
-      </div>
+      </main>
     </>
   );
 }
